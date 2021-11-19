131f68025e36025f0c523f804d3897a266d6a68a
yahoor <mjachorek7@gmail.com>
2021-11-19 20:59:10 +0100

    layers menu

--- frontend/ionic-donos/src/app/const/BaseLayers.ts
+++ frontend/ionic-donos/src/app/const/BaseLayers.ts
@@ -2,12 +2,11 @@ import Layer from 'ol/layer/Layer';
 import TileLayer from 'ol/layer/Tile';
 import SourceOsm from 'ol/source/OSM';
 
+
 export const BaseLayers: Layer[] = [
     new TileLayer({
       zIndex: 0,
-      source: new SourceOsm({
-        url: "https://lat-stage-maps.fream.pl/styles/klokantech-basic/{z}/{x}/{y}.png"
-      })
+      source: new SourceOsm()
     }),
   ]
   
--- frontend/ionic-donos/src/app/pages/map/map.ts
+++ frontend/ionic-donos/src/app/pages/map/map.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Inject, ViewChild, AfterViewInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { DOCUMENT} from '@angular/common';
 import { MapService } from '../../services/map.service';
 
@@ -11,17 +11,21 @@ import { MapService } from '../../services/map.service';
 export class MapPage implements AfterViewInit {
 
   constructor(
+    private menu: MenuController,
     public platform: Platform,
     private mapService: MapService) {}
 
   ngAfterViewInit() {
     
-
     this.mapService.updateSize()
     this.mapService.refreshMap()
 
-
     let map;
+  }
+
+  openLayers(){
+    console.log("open layers")
+    this.menu.open("menuLayers")
   }
 }
 
